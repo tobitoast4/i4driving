e20df6152939b22b823ad27c31c43515dd597888
wjschakel <w.j.schakel@tudelft.nl>
2025-06-19 16:34:47 +0200

    Introduced M-IDM

---
+++ i4driving-ots/src/main/java/org/opentrafficsim/i4driving/tactical/IdmModified.java
@@ -0,0 +1,89 @@
+package org.opentrafficsim.i4driving.tactical;
+
+import org.djunits.unit.SpeedUnit;
+import org.djunits.value.vdouble.scalar.Acceleration;
+import org.djunits.value.vdouble.scalar.Length;
+import org.djunits.value.vdouble.scalar.Speed;
+import org.opentrafficsim.base.parameters.ParameterException;
+import org.opentrafficsim.base.parameters.ParameterTypeAcceleration;
+import org.opentrafficsim.base.parameters.ParameterTypeSpeed;
+import org.opentrafficsim.base.parameters.ParameterTypes;
+import org.opentrafficsim.base.parameters.Parameters;
+import org.opentrafficsim.base.parameters.constraint.NumericConstraint;
+import org.opentrafficsim.road.gtu.lane.perception.PerceptionIterable;
+import org.opentrafficsim.road.gtu.lane.perception.headway.Headway;
+import org.opentrafficsim.road.gtu.lane.tactical.following.AbstractCarFollowingModel;
+import org.opentrafficsim.road.gtu.lane.tactical.following.DesiredHeadwayModel;
+import org.opentrafficsim.road.gtu.lane.tactical.following.DesiredSpeedModel;
+
+/**
+ * Implementation of the M-IDM.
+ * @author wjschakel
+ */
+public class IdmModified extends AbstractCarFollowingModel
+{
+
+    /** Maximum (desired) car-following acceleration. */
+    private static final ParameterTypeAcceleration A = ParameterTypes.A;
+
+    /** Maximum comfortable car-following deceleration. */
+    private static final ParameterTypeAcceleration B = ParameterTypes.B;
+
+    /** Critical speed. */
+    private static final ParameterTypeSpeed VC = new ParameterTypeSpeed("v_c", "Critical speed in IDM",
+            new Speed(37.58, SpeedUnit.KM_PER_HOUR), NumericConstraint.POSITIVE);
+
+    /**
+     * Constructor.
+     * @param desiredHeadwayModel desired headway model
+     * @param desiredSpeedModel desired speed model
+     */
+    public IdmModified(final DesiredHeadwayModel desiredHeadwayModel, final DesiredSpeedModel desiredSpeedModel)
+    {
+        super(desiredHeadwayModel, desiredSpeedModel);
+    }
+
+    @Override
+    public String getName()
+    {
+        return "M-IDM";
+    }
+
+    @Override
+    public String getLongName()
+    {
+        return "Modified Intelligent Driver Model";
+    }
+
+    @Override
+    protected Acceleration followingAcceleration(final Parameters parameters, final Speed speed, final Speed desiredSpeed,
+            final Length desiredHeadway, final PerceptionIterable<? extends Headway> leaders) throws ParameterException
+    {
+        double v = speed.si;
+        double vRat = v / desiredSpeed.si;
+        double a = parameters.getParameter(A).si;
+
+        if (leaders.isEmpty())
+        {
+            return Acceleration.instantiateSI(a * (1.0 - (vRat * vRat * vRat * vRat)));
+        }
+
+        double ss = desiredHeadway.si + 0;
+        double s = leaders.first().getDistance().si;
+        double sRat = ss / s;
+
+        if (ss <= s)
+        {
+            return Acceleration.instantiateSI(a * (1.0 - (vRat * vRat * vRat * vRat) - (sRat * sRat)));
+        }
+        else if (v <= parameters.getParameter(VC).si)
+        {
+            return Acceleration.instantiateSI(a * (1.0 - (sRat * sRat)));
+        }
+
+        double b = parameters.getParameter(B).si;
+
+        return Acceleration.instantiateSI(Math.min(a * (1.0 - (sRat * sRat)), -b));
+    }
+
+}
